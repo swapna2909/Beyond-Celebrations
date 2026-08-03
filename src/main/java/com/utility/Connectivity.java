96a35f48b7c73550e9c87af11421e482153faa16
Sneha252004 <snehasada2004@gmail.com>
2026-08-03 15:48:40 +0530

    received all data

--- src/main/java/com/utility/Connectivity.java
+++ src/main/java/com/utility/Connectivity.java
@@ -9,7 +9,7 @@ public class Connectivity {
 		Connection con=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/BeyondCelebrations", "root", "tiger");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/BeyondCelebrations", "root", "snehananda");
 		}
 		catch (ClassNotFoundException | SQLException  e) {
 			// TODO Auto-generated catch block
